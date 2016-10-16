b96c7211534186790ecd79f0ff36c01f391d03aa
Ratushnyak <kosta-mars19@ukr.net>
2016-10-16 13:49:29 +0300

    Добавлено несколько функций

--- box/src/main/java/ua/stqa/pft/box/MyTest.java
+++ box/src/main/java/ua/stqa/pft/box/MyTest.java
@@ -1,8 +1,17 @@
 package ua.stqa.pft.box;
 
-public class MyTest{
+public class MyTest {
 
-     public static void main(String[] args){
-	System.out.println("Hello World!!!");
-}
+    public static void main(String[] args) {
+        hello("world");
+        hello("user");
+    }
+
+    public static void hello(String somebody){
+        System.out.println("Hello, " + somebody + "!");
+    }
+
+    public static double area(double l){return l * l;}
+
+    public static double area(double a, double b){return a*b;}
 }
