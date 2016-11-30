62deb70b6350a7b8c5f4ff9a90b0ec9f3393a664
Ratushnyak <kosta-mars19@ukr.net>
2016-11-30 23:21:48 +0200

    30112016

--- box/src/main/java/ua/stqa/pft/box/MyTest.java
+++ box/src/main/java/ua/stqa/pft/box/MyTest.java
@@ -5,13 +5,15 @@ public class MyTest {
     public static void main(String[] args) {
         hello("world");
         hello("user");
+
+        Square s = new Square(5);
+        System.out.println("Площадь квадрата, со стороной " + s.l + " = " + s.area());
+
+        Rectangle r = new Rectangle(4, 6);
+        System.out.println("Площадь квадрата, со сторонами " + r.a + " и " + r.b + " = " + r.area());
     }
 
     public static void hello(String somebody){
         System.out.println("Hello, " + somebody + "!");
     }
-
-    public static double area(double l){return l * l;}
-
-    public static double area(double a, double b){return a*b;}
 }
---
+++ box/src/main/java/ua/stqa/pft/box/Square.java
@@ -0,0 +1,14 @@
+package ua.stqa.pft.box;
+
+/**
+ * Created by asus on 16.10.2016.
+ */
+public class Square {
+    public double l;
+
+    public Square(double l){
+        this.l = l;
+    }
+
+    public double area(){return l * l;}
+}
